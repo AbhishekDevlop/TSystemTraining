bbe8704ea9cb1c16b96d4e4a8ce9208e96927308
AbhishekDevlop <101594544+AbhishekDevlop@users.noreply.github.com>
2022-03-15 11:18:04 +0530

    Add project files.

---
+++ DemoBasic/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoBasic
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            //Department obj = new Department();    
+          
+            //Department dept = new Department();
+
+            //Console.WriteLine(dept.GetData());
+            //Department dept2 = new Department(10, "Admin", "Mumbai");
+            //Console.WriteLine(dept2.GetData());
+            //obj.asigndata();
+            //Console.WriteLine(obj.GetData());   
+
+            Student stud = new Student();
+
+            Console.WriteLine(stud.GetData());
+
+            Student stud2 = new Student(10,"Electrical","Abhishek");
+
+            Console.WriteLine(stud2.GetData());
+
+        }
+    }
+}
